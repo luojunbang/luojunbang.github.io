0a13248445f83b330cb50a4364528c08f4e6091b
luojunbang <joe@luoMbp.local>
2021-07-19 14:32:45 +0800

    init project

---
+++ dev/src/components/loInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="text" v-model="inputText" @input="input" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: "",
+    };
+  },
+  methods: {
+    input() {
+      this.$emit("input", this.inputText);
+    },
+  },
+};
+</script>
+
+<style></style>
