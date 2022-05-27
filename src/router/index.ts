7a9cc41c2e7c0ffac518fef2a83830abf263093c
luojunbang <luojunbang@foxmail.com>
2022-05-27 10:57:53 +0800

    add some

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,6 +34,14 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/CssDisplay/index.vue')
+  },
+  {
+    path: '/demo',
+    name: 'CssDisplay',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/demo/index.vue')
   }
 ]
 
