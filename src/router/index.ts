8f8bcf414145fb7a277e5e012cffa03aadf6c67b
luojunbang <luojunbang@foxmail.com>
2022-07-08 09:20:39 +0800

    router import

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import sub from '@/layout/sub.vue'
 import Appmain from '@/layout/Appmain.vue'
 import dashboard from '@/views/Dashboard/dashboard.vue'
 // import { routeAutoLink } from 'lo-utils/src/routerHelper'
+// import { routeAutoLink } from './routerImport'
 import { routeAutoLink } from 'lo-utils'
 
 const routes: Array<RouteRecordRaw> = [
@@ -25,30 +26,26 @@ const routes: Array<RouteRecordRaw> = [
  * 深度遍历
  * @param viewsRootPath
  */
-const config = {
-  'Todos/index.vue': { meta: {}, params: ':id' },
-  'Page1/index.vue': { meta: { title: 'Page1Index' }, params: ':userId' },
-  'Page1/SubPage2/SubPage2.vue': { meta: {}, params: ':userId' },
-  'Page1/SubPage2/Page2/index.vue': { meta: {} },
+export const config = {
+  'Todos/index.vue': { meta: {}, params: ':id?' },
+  'Example/Page1/SubPage2/SubPage2.vue': { meta: { title: '改名字' }, params: ':userId?' },
+  'Example/Page1/SubPage2/Page2/index.vue': { meta: {} },
 }
 
-const routePath = require
-  .context('../views', true, /\.vue$/)
-  .keys()
-  .filter(i => !i.includes('Charts/'))
+const routePath = require.context('../views', true, /\.vue$/).keys()
 
-export const navRoutePath: string[][] = routePath
-  .map(route => route.replace(/^\.\//, '').split('/'))
-  .filter(route => {
-    const routeAry: string[] = route.slice(-2)
-    if (routeAry.length == 1) return true
-    return routeAry[1] === 'index.vue' || routeAry[0].toLocaleLowerCase() === routeAry[1].replace(/\.vue$/, '').toLocaleLowerCase()
-  })
+export const navRoutePath: string[] = [...routePath].filter(route => {
+  const routeAry: string[] = route.split('/').slice(-2)
+  if (routeAry.length == 1) return true
+  return routeAry[1] === 'index.vue' || routeAry[0].toLocaleLowerCase() === routeAry[1].replace(/\.vue$/, '').toLocaleLowerCase()
+})
 
 // const routePath = Object.keys(import.meta.globEager('../views/*.vue')).map(i => i.replace(/^\.\.\//g, ''))
 // console.log(routePath)
 
 const routesAuto = routeAutoLink(routePath, [main, Appmain, sub], config)(path => () => import(/*webpackChunkName:"[request]"*/ `../views/${path.replace(/\.vue$/, '')}.vue`))
+
+console.log(routesAuto)
 
 const router = createRouter({
   history: createWebHashHistory(),
