9b9ed71820720f7bed2785bb2f2bde867370eb22
luojunbang <luojunbang@foxmail.com>
2022-07-19 13:52:48 +0800

    swiper

--- src/views/Carousel/components/Swiper.vue
+++ src/views/Carousel/components/Swiper.vue
@@ -1,13 +1,12 @@
 <template>
+  <div>activeIndex:{{ activeIndex }}</div>
+  <div id="nextIndex">nextIndex:{{ nextIndex }}</div>
+  <div>lastIndex:{{ lastIndex }}</div>
+  <div>diretion:{{ diretion }}</div>
+  <div id="ready" style="width: 0; height: 0; display: none">ready:{{ ready }}</div>
   <div class="swiper-container">
-    <div
-      class="swiper-item text-center pd-t-lg"
-      :style="initing && index == nextIndex ? { ...cpStyle(index), ...nextPositionStyle } : cpStyle(index)"
-      v-for="(item, index) in swiperList"
-      :key="index"
-      :class="{ animating: animating(index) }"
-    >
-      item:{{ index }}-{{ item.id }}
+    <div class="swiper-item text-center pd-t-lg" :style="cpStyle(index)" v-for="(item, index) in swiperList" :key="index" :class="{ animating: ready && activeList.includes(index) }">
+      <div>{{ index }}<br />{{ item.id }}<br />{{ ready ? 'T' : 'F' }}</div>
     </div>
   </div>
   <div class="text-center">
@@ -17,48 +16,111 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from '@vue/reactivity'
+import { computed, unref } from '@vue/reactivity'
 import { r } from 'lo-utils'
-import { onMounted, reactive, ref } from 'vue'
-const swiperList = reactive(new Array(2).fill(0).map((i, idx) => ({ id: r() })))
+import { CSSProperties, nextTick, onMounted, reactive, ref } from 'vue'
+const swiperList = reactive(new Array(11).fill(0).map((i, idx) => ({ id: r(2) })))
 
 const calc = i => (i + swiperList.length) % swiperList.length
 
 const nextIndex = ref(-1)
 const activeIndex = ref(0)
-const lastIndex = ref(swiperList.length - 1)
+const lastIndex = ref(-1)
 const diretion = ref('')
+
+const nextStyle = reactive({
+  transform: '',
+})
+
+const activeList = computed(() => {
+  return [lastIndex.value, ...new Array(IN_SHOW_COUNT).fill(0).map((i, idx) => activeIndex.value + idx)].map(calc)
+})
+
 function next() {
   diretion.value = 'next'
-  nextIndex.value = calc(activeIndex.value + 1 + IN_SHOW_COUNT - 1)
+  nextIndex.value = calc(activeIndex.value + IN_SHOW_COUNT)
+  ready.value = false
+  animating.value = false
+  nextStyle.transform = `translateX(${100 * 2}%)`
   setTimeout(() => {
+    console.log('ready:', ready.value, document.getElementById('ready')?.innerText)
+    ready.value = true
     lastIndex.value = activeIndex.value
     activeIndex.value = calc(activeIndex.value + 1)
-  }, 300)
+  })
 }
 function prev() {
   diretion.value = 'prev'
   nextIndex.value = calc(activeIndex.value - 1)
-  setTimeout(() => {
+  ready.value = false
+  nextStyle.transform = `translateX(${100 * -1}%)`
+  nextTick(() => {
+    console.log('ready:', ready.value, document.getElementById('ready')?.innerText)
+    ready.value = true
     lastIndex.value = activeIndex.value
     activeIndex.value = calc(activeIndex.value - 1)
-  }, 300)
+  })
 }
 
-const wait = 0.3
+const ready = ref(true)
+const animating = ref(false)
+const cpAnimating = computed(() => {
+  return index => {
+    return ready.value
+  }
+})
+const cpStyle = computed(() => {
+  // console.log('activeIndex:', activeIndex.value)
+  // console.log('lastIndex:', lastIndex.value)
+  // console.log('diretion:', diretion.value)
+  // console.log('diretion:', ready.value)
+  return index => {
+    if (index === activeIndex.value) {
+      console.log(activeIndex.value)
+    }
+    if (!ready.value && index == nextIndex.value) {
+      console.log('nextIndex computedindex:', index)
+      if (diretion.value == 'prev') return { transform: `translateX(${100 * -1}%)` }
+      else if (diretion.value == 'next') return { transform: `translateX(${100 * IN_SHOW_COUNT + 1}%)` }
+    }
+    const ans: CSSProperties = {}
+    let list: number[] = []
+    let base = 0
+    if (ready.value && diretion.value == 'prev') {
+      // base = -1
+      // activeIndex.value - 1,
+      list = [activeIndex.value, ...new Array(IN_SHOW_COUNT).fill(0).map((i, idx) => lastIndex.value + idx)].map(calc)
+    } else if (ready.value && diretion.value == 'next') {
+      base = -1
+      list = [lastIndex.value, ...new Array(IN_SHOW_COUNT).fill(0).map((i, idx) => activeIndex.value + idx)].map(calc)
+    } else list = [...new Array(IN_SHOW_COUNT).fill(0).map((i, idx) => activeIndex.value + idx)].map(calc)
+    if (list.indexOf(index) != -1) {
+      console.log('list computedindex:', index)
+      ans.transform = `translateX(${100 * (list.indexOf(index) + base)}%)`
+    } else {
+      ans.transform = `translateX(${100 * IN_SHOW_COUNT + 1}%)`
+      ans.display = [lastIndex.value, ...new Array(IN_SHOW_COUNT).fill(0).map((i, idx) => activeIndex.value + idx)].map(calc).includes(index) ? 'block' : 'none'
+    }
+    return ans
+  }
+})
+
+const wait = ref(0.5)
+const cpWait = computed(() => {
+  return wait.value + 's'
+})
 let timer
 const handleMove = () => {
-  timer = setTimeout(() => {
+  timer = setInterval(() => {
     next()
-    handleMove()
-  }, wait * 1000)
+  }, wait.value * 1000)
 }
 
 onMounted(() => {
-  // handleMove()
+  handleMove()
 })
 
-const IN_SHOW_COUNT = 1 // Count
+const IN_SHOW_COUNT = 10 // Count
 
 const SAFE_COUNT = 1
 </script>
@@ -67,18 +129,18 @@ const SAFE_COUNT = 1
 .swiper-container {
   position: relative;
   width: 100%;
-  height: 90%;
+  height: 300px;
 }
 .swiper-item {
   // width: 300px;
-  width: 100%;
+  width: 10%;
   border: 1px solid black;
   height: 100%;
   position: absolute;
   left: 0;
   top: 0;
   &.animating {
-    transition: all 0.3s linear;
+    transition: all v-bind(cpWait) linear;
   }
 }
 </style>
--- src/views/Carousel/index.vue
+++ src/views/Carousel/index.vue
@@ -5,6 +5,11 @@
         <!-- <swiper-item v-for="(item, index) in list" :key="index">item:{{ index }}</swiper-item> -->
       </swiper>
     </div>
+    <div>reset:{{ reset }}</div>
+    <div class="relative border" style="height: 300px; width: 300px">
+      <div ref="ball" class="ball" id="reset" :style="style" :class="{ animating: reset, 'ball-green': !reset }">{{ reset }}</div>
+    </div>
+    <button @click="start">123</button>
   </div>
 </template>
 
@@ -12,8 +17,37 @@
 import Swiper from './components/Swiper.vue'
 import SwiperItem from './components/SwiperItem.vue'
 import { r } from 'lo-utils'
-import { reactive } from 'vue'
+import { computed, reactive, ref, nextTick, watchPostEffect, watchSyncEffect, onMounted } from 'vue'
+import { watch } from 'fs'
 const list = reactive(new Array(20).fill(0).map((i, idx) => ({ id: r() })))
+const reset = ref(false)
+const real = ref(false)
+const ball = ref()
+const style = reactive({ left: '0px', top: '0px' })
+console.log(ball)
+
+onMounted(() => {
+  ball.value.addEventListener('transitionend', () => {})
+})
+
+function start() {
+  style.left = '100px'
+  style.top = '0px'
+  reset.value = false
+  setTimeout(() => {
+    console.log('transitionend')
+    reset.value = true
+    real.value = true
+    style.top = '100px'
+  })
+}
+watchPostEffect(() => {
+  console.log('reset watchPostEffect:', reset.value)
+})
+
+watchSyncEffect(() => {
+  console.log('reset watchSyncEffect:', reset.value)
+})
 </script>
 
 <style lang="scss" scoped>
@@ -23,7 +57,20 @@ div.carousel {
   width: 600px;
   margin: 0 auto;
   margin-top: 200px;
-  height: 300px;
+  height: 500px;
   overflow: hidden;
+}
+.animating {
+  transition: all 0.3s linear;
+}
+.ball-green {
+  background-color: green !important;
+}
+.ball {
+  width: 10px;
+  height: 10px;
+  border: 1px solid black;
+  background-color: red;
+  position: absolute;
 }
 </style>
