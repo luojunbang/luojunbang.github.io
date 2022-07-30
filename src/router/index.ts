1d7c58fc779f69b96eba7bb31d465ae1835d827f
luojunbang <luojunbang@foxmail.com>
2022-07-30 15:13:00 +0800

    store one

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,14 +10,14 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     redirect: '/dashboard',
   },
-  {
-    path: '/dashboard',
-    component: dashboard,
-  },
-  {
-    path: '/demo',
-    component: () => import('@/views/Example/demo/index.vue'),
-  },
+  // {
+  //   path: '/dashboard',
+  //   component: dashboard,
+  // },
+  // {
+  //   path: '/demo',
+  //   component: () => import('@/views/Example/demo/index.vue'),
+  // },
 ]
 
 /**
@@ -30,15 +30,15 @@ export const config = {
   'Example/Page1/SubPage2/Page2/index.vue': { meta: {} },
 }
 
-// const routePath = require.context('../views/', true, /\.vue$/).keys()
+const routePath = require.context('../views/', true, /\.vue$/).keys()
 
-// const routesAuto = routeAutoLink(routePath, [main, Appmain, sub], config)(path => () => import(/*webpackChunkName:"[request]"*/ `../views/${path.replace(/\.vue$/, '')}.vue`))
+const routesAuto = routeAutoLink(routePath, [main, Appmain, sub], config)(path => () => import(/*webpackChunkName:"[request]"*/ `../views/${path.replace(/\.vue$/, '')}.vue`))
 
 // console.log(routesAuto)
 
 const router = createRouter({
   history: createWebHashHistory(process.env.BASE_URL),
-  routes: routes, //[...routes, ...routesAuto.map(i => ({ ...i, path: '/' + i.path }))], //
+  routes: [...routes, ...routesAuto.map(i => ({ ...i, path: '/' + i.path }))], //
 })
 
 export default router
