1b07babb8e18edaf789fc11b6e3a986850681926
luojunbang <luojunbang@foxmail.com>
2022-08-02 11:19:14 +0800

    编译step1

--- packages/vue3-web/build/webpack.config.js
+++ packages/vue3-web/build/webpack.config.js
@@ -1,25 +1,31 @@
 const { VueLoaderPlugin } = require('vue-loader')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { DefinePlugin } = require('webpack')
-
-const { resolve } = require('path')
+const { fmtDateTime } = require('lo-utils')
+const path = require('path')
 const { ProgressPlugin } = require('webpack')
-console.log(process.env.NODE_ENV, ':', resolve(__dirname, '../src/main.ts'))
+const d = Date.now()
+const chalk = require('chalk')
+
+const resolve = p => path.resolve(__dirname, p)
+
+console.warn('NODE_ENV:', process.env.NODE_ENV, ':', resolve(__dirname, '../src/main.ts'))
+console.time('Start Time:', fmtDateTime(d))
 const config = {
   mode: process.env.NODE_ENV,
-  entry: { app: [resolve(__dirname, '../src/main.ts')] },
+  entry: { app: [resolve('../src/main.ts')] },
   output: {
-    path: resolve(__dirname + '../dist/'),
+    path: resolve('../dist/'),
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, '../src'),
+      '@': resolve('../src'),
     },
     extensions: ['.ts', '.tsx', '.vue', '.js'],
   },
   devServer: {
     // port: 9988,
-    static: resolve(__dirname, '../dist/'),
+    static: resolve('../dist/'),
     hot: true,
   },
   externals: {
@@ -50,7 +56,7 @@ const config = {
             loader: 'ts-loader',
             options: {
               appendTsSuffixTo: [/\.vue$/],
-              transpileOnly:true
+              transpileOnly: true,
             },
           },
         ],
@@ -61,10 +67,13 @@ const config = {
     new VueLoaderPlugin(),
     new HTMLWebpackPlugin({
       BASE_URL: process.env.BASE_URL,
-      template: resolve(__dirname, '../public/index.html'),
+      template: resolve('../public/index.html'),
     }),
     new ProgressPlugin((percentage, message, ...args) => {
-      console.info(percentage, message, ...args)
+      console.info(chalk.green((percentage * 100).toFixed(0) + '%'), message, ...args)
+      if (percentage === 1) {
+        console.log("\33[2J");
+      }
     }),
     new DefinePlugin({
       'process.env': {
