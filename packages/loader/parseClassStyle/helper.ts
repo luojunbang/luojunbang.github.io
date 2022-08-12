70d986781b5658d1f92ed9472cc338fc4847d4a0
luojunbang <luojunbang@foxmail.com>
2022-08-12 11:22:42 +0800

    cache

--- packages/loader/parseClassStyle/helper.ts
+++
@@ -1,6 +0,0 @@
-const chalk = require('chalk')
-
-module.exports = function loader(content) {
-  console.log(this.resourcePath + this.resourceQuery, chalk.yellow('helper'))
-  return content
-}
--- packages/loader/parseClassStyle/index.js
+++ packages/loader/parseClassStyle/index.js
@@ -2,9 +2,9 @@ const chalk = require('chalk')
 const loaderUtils = require('loader-utils')
 const compiler_sfc_1 = require('vue/compiler-sfc')
 
-const qs = require('querystring')
+const { getCssProperty } = require('./helper')
 
-const cache = {}
+const qs = require('querystring')
 
 function deepPriority(root, fn) {
   if (Array.isArray(root.children) && root.children.length > 0) {
@@ -15,18 +15,15 @@ function deepPriority(root, fn) {
   }
 }
 
-function getCssProperty(key) {
-  const config = {
-    font: 'font-size',
-  }
-  return config[key]
-}
-
 function parseClass(className, prefix) {
   let style = ''
   try {
-    const [attr, value] = className.replace(prefix, '').split('-')
-    style = `.${className} { ${getCssProperty(attr)} : ${value}px; }\n`
+    const [attr, ...value] = className.replace(prefix, '').split('-')
+    style = `.${className} { ${getCssProperty(attr)
+      .split(',')
+      .map(prop => `${prop} : ${value.join(' ')};`)
+      .join('\n')}
+    }`
   } catch (e) {
     console.log(e)
   }
@@ -36,19 +33,17 @@ function parseClass(className, prefix) {
 const wrapStyleBlock = block => `\n  <style scoped> ${block}</style> \n`
 
 module.exports = function loader(content) {
+  const d = Date.now()
   const ctx = this
   const { prefix = '$' } = loaderUtils.getOptions(this)
   const isParseClass = klass => {
-    return new RegExp(`\^\\${prefix}[\\w]+\-[\\d]+(px|%|rem|em|vw|vh)?`).test(klass)
+    return new RegExp(`\^\\${prefix}[\\w]+\-`).test(klass)
   }
   const { mode, target, sourceMap, rootContext, resourcePath, resourceQuery = '' } = ctx
   const incomingQuery = qs.parse(resourceQuery.slice(1))
   if (!(!resourceQuery || incomingQuery.type === 'style')) return content
-  if (incomingQuery.id && Reflect.has(cache, incomingQuery.id)) {
-    return content + wrapStyleBlock(cache[incomingQuery.id])
-  }
   const { descriptor, errors } = compiler_sfc_1.parse(content, {})
-  resourcePath.includes('demo')
+  if (incomingQuery.type === 'style' && incomingQuery.index < descriptor.styles.length) return content
   const styles = new Set()
   deepPriority(descriptor.template.ast, node => {
     const classProps = node.props?.find(i => i.name === 'class')
@@ -58,7 +53,5 @@ module.exports = function loader(content) {
       })
   })
   const styleBlock = Array.from(styles).reduce((rs, klass) => rs + parseClass(klass, prefix), '')
-  cache[incomingQuery.id] = styleBlock
-  console.log(cache[incomingQuery.id])
-  return content + wrapStyleBlock(cache[incomingQuery.id])
+  return content + wrapStyleBlock(styleBlock)
 }
