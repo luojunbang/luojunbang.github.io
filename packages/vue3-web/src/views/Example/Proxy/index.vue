45919bfcbcbcd150c43831335b6826215e7a1b4a
luojunbang <luojunbang@foxmail.com>
2023-04-27 11:49:11 +0800

    feat: 增加defineProperty和Proxy响应式

--- packages/vue3-web/src/views/Example/Proxy/index.vue
+++ packages/vue3-web/src/views/Example/Proxy/index.vue
@@ -5,6 +5,7 @@
 <script lang="ts" setup>
 import { onMounted, reactive } from 'vue'
 import { Observe as ObserveV2 } from './observe_v2'
+import { observe as ObserveV3 } from './observe_v3'
 
 const personV2: Record<string, any> = {
   name: 'personV2',
@@ -29,18 +30,26 @@ const log = (type: string, ...args: any[]) => {
 
 onMounted(() => {
   new ObserveV2(personV2)
-  personV2.name
-  personV2.name += '__Changed'
-  personV2.info
-  personV2.info.age = { old: '99', new: '100' }
-  personV2.info.age.old
-  personV2.assets.push('1')
-  personV2.assets.unshift('0')
-  personV2.assets
-  personV2.assets.pop()
-  personV2.assets
-  personV2.location
-  personV2.location = ['gz', 'sz']
-  personV2.location
+  ;[personV2, ObserveV3(personV3)].forEach(target => {
+    console.log('-------------')
+    target.name
+    target.name += '__Changed'
+    console.log('-------------')
+    target.info
+    target.info.age = { old: '99', new: '100' }
+    target.info.age.old++
+    console.log('-------------push')
+    target.assets.push('1')
+    console.log('-------------unshift')
+    target.assets.unshift('0')
+    console.log('-------------get')
+    target.assets
+    console.log('-------------pop')
+    target.assets.pop()
+    console.log('-------------')
+    target.location
+    target.location = ['gz', 'sz']
+    target.location[0] = 1
+  })
 })
 </script>
--- packages/vue3-web/src/views/Example/Proxy/observe_v3.ts
+++ packages/vue3-web/src/views/Example/Proxy/observe_v3.ts
@@ -1,1 +1,42 @@
-class Observe {}
+import { isJSType } from 'lo-utils'
+
+export function generateProxy(obj: any) {
+  const handler = {
+    get(obj: any, key: any) {
+      console.log('PROXY GET: ', key, obj[key])
+      return obj[key]
+    },
+    set(obj: any, key: any, value: any) {
+      console.log('PROXY SET: ', key, obj[key], '->', value)
+      if (isJSType(value, 'object')) {
+        obj[key] = observe(value)
+      } else if (isJSType(value, 'array')) {
+        obj[key] = generateProxy(value)
+      } else {
+        obj[key] = value
+      }
+      return true
+    },
+  }
+  return new Proxy(obj, handler)
+}
+
+export function observe(obj: Record<string, any>) {
+  Object.keys(obj).forEach(k => {
+    if (isJSType(obj[k], 'object')) {
+      obj[k] = generateProxy(obj[k])
+      observe(obj[k])
+    }
+    if (isJSType(obj[k], 'array')) {
+      obj[k] = generateProxy(obj[k])
+    }
+  })
+  return generateProxy(obj)
+}
+
+// get set has
+// defineProperty deleteProperty
+// getPrototype setPrototypeOf
+// new apply
+// ownKeys
+// preventExtensions isExtensiable
